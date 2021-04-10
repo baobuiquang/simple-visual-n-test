fc837b5e79b232ce0a6df8184c241947ec6465c6
Bui Quang Bao <60503568+buiquangbao@users.noreply.github.com>
2021-04-10 17:43:44 +0700

    test

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@
 // d: dialog
 // s: sound effect
 // t: blip tone
+// n: note
 
 var characters = [
     { name: "" },
@@ -13,28 +14,43 @@ var characters = [
     { name: "Character 4" }
 ];
 
+var charImgs = [
+    { link: "" },
+    { link: "./assets/char1.png" },
+    { link: "./assets/char2.png" },
+    { link: "./assets/char3.png" },
+    { link: "./assets/char4.png" }
+];
+
 var backgrounds = [
     { link: "" },
-    { link: "https://buiquangbao.github.io/bg1.png" },
-    { link: "https://buiquangbao.github.io/bg2.png" },
-    { link: "https://buiquangbao.github.io/bg3.png" }
+    { link: "./assets/bg1.png" },
+    { link: "./assets/bg2.png" },
+    { link: "./assets/bg3.png" },
+    { link: "./assets/bg4.png" }
+];
+
+var eventImgs = [
+    { link: "./assets/transparent.png" },
+    { link: "./assets/img1.png" },
+    { link: "./assets/img2.png" },
 ];
 
 var musics = [
     { link: "" },
-    // { link: "https://buiquangbao.github.io/music1.mp3" },
-    // { link: "https://buiquangbao.github.io/music2.mp3" },
-    // { link: "https://buiquangbao.github.io/music3.mp3" }
+    { link: "https://buiquangbao.github.io/music1.mp3" },
+    { link: "https://buiquangbao.github.io/music2.mp3" },
+    { link: "https://buiquangbao.github.io/music3.mp3" }
 ];
 
 var allDialogs = {
     eventName1: [
         { next: "eventName2" },
-        { m: 1, t: 1, b: 1, c: 1, d: "This is sentence 0." },
+        { m: 1, t: 1, b: 1, c: 1, d: "This is sentence 0.", n: "This is a note 1!" },
         { d: "This is sentence 1." },
-        { d: "This is sentence 2." },
+        { d: "This is sentence 2.", i: 1 },
         { b: 3, c: 4, d: "This is sentence 3." },
-        { d: "This is sentence 4." },
+        { d: "This is sentence 4.", n: "This is a note 2!" },
         { d: "This is sentence 5." }
     ],
     eventName2: [
@@ -44,14 +60,14 @@ var allDialogs = {
     eventName3: [
         { next: "eventName1" },
         { m: 2, t: 2, b: 2, c: 2, d: "Route A: This is sentence 0." },
-        { d: "Route A: This is sentence 1." },
-        { d: "Route A: This is sentence 2." },
+        { d: "Route A: This is sentence 1.", i: 2 },
+        { d: "Route A: This is sentence 2.", n: "This is a note 3!" },
         { d: "Route A: This is sentence 3." }
     ],
     eventName4: [
         { next: "eventName1" },
-        { m: 3, t: 3, b: 3, c: 3, d: "Route B: This is sentence 0." },
-        { d: "Route B: This is sentence 1." },
+        { m: 3, t: 3, b: 4, c: 3, d: "Route B: This is sentence 0." },
+        { d: "Route B: This is sentence 1.", n: "This is a note 4!" },
         { d: "Route B: This is sentence 2." }
     ]
 };
@@ -116,9 +132,16 @@ function userClick() {
             document.getElementById("character-name").innerHTML =
                 characters[currentEvent[i].c].name;
         }
+        if (currentEvent[i].c != undefined) {
+            document.getElementById("character-img").src =
+                charImgs[currentEvent[i].c].link;
+        }
         if (currentEvent[i].b != undefined) {
             document.getElementById("background-img").src =
                 backgrounds[currentEvent[i].b].link;
+        }
+        if (currentEvent[i].n != undefined) {
+            document.getElementById("note").innerHTML = currentEvent[i].n;
         }
         if (currentEvent[i].m != undefined) {
             try {
@@ -126,6 +149,13 @@ function userClick() {
             } catch (e) {
                 console.log(e);
             }
+        }
+        if (currentEvent[i].i != undefined) {
+            document.getElementById("event-img").src =
+                eventImgs[currentEvent[i].i].link;
+        } else {
+            document.getElementById("event-img").src =
+                eventImgs[0].link; //Transparent
         }
         displayDialogDone = false;
         let str = "";
@@ -158,6 +188,7 @@ function selectChoice(choice) {
         document.getElementById("choice-wrap").style.opacity = "0";
         document.getElementById("choice-" + i.toString()).style.opacity = "0";
     }
+    document.getElementById("note").innerHTML = "Your choice: " + currentEvent[choice].choice;
     nextEvent(choice);
 }
 
